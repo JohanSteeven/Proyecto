5912ed61f624cc284e0855b4cff7fd66bbdcedaa
JohanSteeven <105741760+JohanSteeven@users.noreply.github.com>
2025-05-15 20:29:52 -0500

    Cambio parámetro api

--- app/routers/evaluacion.py
+++ app/routers/evaluacion.py
@@ -1,5 +1,4 @@
-from fastapi import APIRouter, HTTPException
-from pydantic import BaseModel
+from fastapi import APIRouter, HTTPException, Query
 from app.services import (
     evaluador_ip,
     autenticacion,
@@ -11,21 +10,18 @@ from app.services import (
 
 router = APIRouter(prefix="/evaluacion", tags=["evaluacion"])
 
-class EvaluacionInput(BaseModel):
-    ip_o_dominio: str
-
 @router.post("/")
-def evaluar(input_data: EvaluacionInput):
+def evaluar(ip_o_dominio: str = Query(..., description="IP o dominio a evaluar")):
     try:
         resultado = {
-            "ip_o_dominio": input_data.ip_o_dominio,
+            "ip_o_dominio": ip_o_dominio,
             "evaluacion": {
-                "escaneo_red": evaluador_ip.evaluar(input_data.ip_o_dominio),
-                "autenticacion": autenticacion.evaluar(),
-                "acceso_logico": acceso_logico.evaluar(),
-                "logs": seguridad_logs.evaluar(),
-                "criptografia": criptografia.evaluar(),
-                "aplicaciones": apps_seguras.evaluar(),
+                "escaneo_red": evaluador_ip.evaluar(ip_o_dominio),
+                "autenticacion": autenticacion.evaluar(ip_o_dominio),
+                "acceso_logico": acceso_logico.evaluar(ip_o_dominio),
+                "logs": seguridad_logs.evaluar(ip_o_dominio),
+                "criptografia": criptografia.evaluar(ip_o_dominio),
+                "aplicaciones": apps_seguras.evaluar(ip_o_dominio),
             }
         }
         return resultado
