8b4dfbaf89fc27562df8221474f201b4cdc8e2cf
JohanSteeven <johanbano@gmail.com>
2025-05-22 12:54:41 -0500

    Interfaz web simple

--- app/main.py
+++ app/main.py
@@ -1,5 +1,9 @@
 from fastapi import FastAPI
 from app.routers import evaluacion
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi import Request
+templates = Jinja2Templates(directory="app/templates")
 
 app = FastAPI(
     title="API Evaluación Automática de Seguridad",
@@ -8,3 +12,7 @@ app = FastAPI(
 )
 
 app.include_router(evaluacion.router)
+
+@app.get("/", response_class=HTMLResponse)
+async def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
